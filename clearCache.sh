2f8f28a9768e5d64b7f651055f1fa9595f837403
Omar <omarmesquita@eq.ufrj.br>
2023-02-22 18:28:00 -0300

    Explained script's purpose

--- clearCache.sh
+++ clearCache.sh
@@ -1,6 +1,6 @@
 # Script to clean the testing environment
 
-
+# Clears user's input files
 cd emission_files 
 rm * 
 cd ..
@@ -17,9 +17,14 @@ cd refractive_index_files
 rm * 
 cd ..
 
+
+# Clears generated data (graphs, .dat files, etc)
 cd result 
 rm -rf * 
 cd ..
 
+
+# Clears application's cache and compressed file 
+# which contains the user's data
 rm -rf __pycache__ 
 rm mydata.zip
